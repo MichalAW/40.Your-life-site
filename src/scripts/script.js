44192c840444d0a72e6c635c2bb8001f33761a31
Michał Wanta <michal.wanta@euco.pl>
2019-11-20 23:20:43 +0100

    add new patrails

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,95 +1,31 @@
-// toggle menu sidebar
-var toggle = document.getElementById("toggle");
-// obsluga sidebara
-var links = document.querySelectorAll(".sidebar a");
-var sections = document.querySelectorAll("main section");
+var links = document.querySelectorAll('.topbar a');
+var sections = document.querySelectorAll('section');
 
-function toggleSidebar() {
-  document.getElementById("sidebar").classList.toggle("active");
-  document.querySelector("main").classList.toggle("fullwidth");
-  document.querySelector(".bg-alpha").classList.toggle("active");
+function toggleTopbar() {
+	document.getElementById('topbar').classList.toggle('active');
 }
 
-if (toggle) {
-  toggle.addEventListener("click", function() {
-    toggleSidebar();
-  });
-}
-
-// nadanie eventu na wszystkich linkach z sidebara
+// event for all links in topbar
 for (var i = 0; i < links.length; i++) {
-  links[i].addEventListener("click", function() {
-    // usuwanie klasy active ze wszystkich section
-    for (var i = 0; i < sections.length; i++) {
-      sections[i].classList.remove("active");
-    }
+	links[i].addEventListener('click', function () {
 
-    // nadawanie klasy active kliknietej z sidebara sekcji
-    document.querySelector(this.hash).classList.add("active");
-  });
+		// remove class active from all sections
+		for (var i = 0; i < sections.length; i++) {
+			sections[i].classList.remove('active');
+		}
+	});
+
+	toggle.addEventListener("click", function () {
+		toggleTopbar();
+	});
 }
 
-//details datownik
-function refreshDate() {
-  var fromData = document.getElementById("from-data").value;
-  var toData = document.getElementById("to-data").value;
-  var tableDates = document.querySelectorAll(
-    ".details-table tr td:nth-child(2)"
-  );
-
-  for (var i = 0; i < tableDates.length; i++) {
-    console.log(tableDates[i].innerHTML);
-    // if () {
-
-    // }
-  }
-}
-
-function ready() {
-  var el = document.getElementById("bar-chart");
-  if (el) {
-    new Chart(el, {
-      type: "bar",
-      data: {
-        labels: ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"],
-        datasets: [
-          {
-            label: "Signups",
-            backgroundColor: "#56819F",
-            data: [133, 221, 283, 478, 576, 567, 444, 414, 266, 166]
-          },
-          {
-            label: "FTD",
-            backgroundColor: "#FFA200",
-            data: [208, 147, 375, 134, 565, 555, 237, 444, 334, 276]
-          }
-        ]
-      },
-      options: {
-        title: {
-          display: true
-        }
-      }
-    });
-  }
-}
-
-var button = document.querySelector(".btn-details");
-if (button) {
-  button.addEventListener("click", refreshDate);
-}
-
-document.addEventListener("DOMContentLoaded", ready);
-
-//modal jquery
-
-$(document).ready(function() {
-  $(".modal-link").click(function() {
-    $(".modal").show();
-    $(".modal-bg").show();
-  });
-  $(".modal .close").click(function() {
-    $(".modal").hide();
-    $(".modal-bg").hide();
-  });
+// validation of the form
+document.querySelector('.btn-send').addEventListener('click', function () {
+	var textareaVal = document.querySelector('.contact textarea').value;
+	if (textareaVal.length > 0) {
+		return;
+	} else {
+		alert("Your message field is required!");
+	}
 });
